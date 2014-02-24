a5fa358e213b853c70f1dedd4859b2677a040758
Jeroen Meetsma <jeroen@ijsberg.nl>
2014-02-24 20:58:08 +0100

    worked on task executor

---
+++ src/main/java/org/ijsberg/iglu/logging/module/StandardOutLogger.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2011-2013 Jeroen Meetsma - IJsberg
+ *
+ * This file is part of Iglu.
+ *
+ * Iglu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Iglu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Iglu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.ijsberg.iglu.logging.module;
+
+import org.ijsberg.iglu.logging.LogEntry;
+import org.ijsberg.iglu.logging.Logger;
+
+/**
+ * Created by Jeroen on 24-2-14.
+ */
+public class StandardOutLogger implements Logger {
+	@Override
+	public void log(LogEntry entry) {
+		System.out.println(entry.toString());
+	}
+
+	@Override
+	public String getStatus() {
+		return null;
+	}
+
+	@Override
+	public void addAppender(Logger appender) {
+
+	}
+
+	@Override
+	public void removeAppender(Logger appender) {
+
+	}
+}
